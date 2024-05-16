0de108e4c81fc13e25e94ae830545529e12a26a1
MRishabh23 <rmalik@justransform.com>
2024-05-16 12:24:45 +0530

    some summary columns value representation on mode

--- src/components/tracking/summary/summary-table.tsx
+++ src/components/tracking/summary/summary-table.tsx
@@ -17,288 +17,6 @@ import { UserContext } from "@/components/dashboard-layout-component";
 import { useSummaryQuery } from "@/utils/query";
 import { ArrowUpDown } from "lucide-react";
 
-export const columns: ColumnDef<SummaryType>[] = [
-  {
-    id: "carrier",
-    accessorKey: "jtCarrierCode",
-    header: () => <TableHeadCustom className="w-32">Carrier</TableHeadCustom>,
-    cell: ({ row }) => (
-      <TableCellCustom>{row.original.jtCarrierCode}</TableCellCustom>
-    ),
-    meta: {
-      className: "sticky left-0 bg-white",
-    },
-  },
-  {
-    id: "queue",
-    accessorKey: "queueType",
-    header: () => <TableHeadCustom>Queue</TableHeadCustom>,
-    cell: ({ row }) => {
-      const queue = row.original.queueType;
-      const qType = queue.includes("NORMAL")
-        ? "normal"
-        : queue.includes("ADAPTIVE")
-        ? "adaptive"
-        : queue.includes("RNF")
-        ? "rnf"
-        : "";
-
-      return (
-        <TableCellCustom>
-          <Badge className="capitalize bg-stone-500">{qType}</Badge>
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "active",
-    accessorKey: "jtCrawledTotal",
-    header: ({ column }) => {
-      return (
-        <TableHeadCustom
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Active <ArrowUpDown className="ml-2 h-4 w-4" />
-        </TableHeadCustom>
-      );
-    },
-    cell: ({ row }) => (
-      <TableCellCustom>{row.original.jtCrawledTotal}</TableCellCustom>
-    ),
-  },
-  {
-    id: "avg-age",
-    accessorKey: "AvgAge",
-    header: () => <TableHeadCustom>Avg Age</TableHeadCustom>,
-    cell: ({ row }) => {
-      const age = row.original.AvgAge;
-      return (
-        <TableCellCustom
-          className={`${age >= 90 ? "text-red-500" : "text-inherit"}`}
-        >
-          {age} days
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "last-run",
-    accessorKey: "lastRunStartAt",
-    header: () => <TableHeadCustom className="w-32">Last Run</TableHeadCustom>,
-    cell: ({ row }) => (
-      <TableCellCustom>{row.original.lastRunStartAt} ago</TableCellCustom>
-    ),
-  },
-  {
-    id: "duration",
-    accessorKey: "timeDiffInFk",
-    header: () => <TableHeadCustom>Duration</TableHeadCustom>,
-    cell: ({ row }) => {
-      const durDiff = row.original.timeDiffMinutes;
-      return (
-        <TableCellCustom
-          className={`${durDiff >= 90 ? "text-red-500" : "text-inherit"}`}
-        >
-          {row.original.timeDiffInFk}
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "success",
-    accessorKey: "successCount",
-    header: () => <TableHeadCustom className="w-32">Success</TableHeadCustom>,
-    cell: ({ row }) => {
-      return (
-        <TableCellCustom>
-          {row.original.successCount} ({row.original.successRatio}%)
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "rnf",
-    accessorKey: "getReferenceNotFound",
-    header: () => <TableHeadCustom>RNF (404)</TableHeadCustom>,
-    cell: ({ row }) => {
-      const rnfRatio = row.original.getReferenceNotFoundPercentage;
-      const queue = row.original.queueType;
-      return (
-        <TableCellCustom
-          className={`${
-            rnfRatio >= 20 && !queue.includes("RNF")
-              ? "text-red-500"
-              : "text-inherit"
-          }`}
-        >
-          {row.original.getReferenceNotFound}
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "fail",
-    accessorKey: "failCount",
-    header: () => <TableHeadCustom className="w-32">Fail</TableHeadCustom>,
-    cell: ({ row }) => {
-      const failRatio = row.original.failureRatio;
-      return (
-        <TableCellCustom>
-          <TableCellTooltip
-            tip={
-              <div>
-                <p>Sending Failure: {row.original.toFKFailedNotSent}</p>
-                <p>API/Scraping Failure: {row.original.toFKFailedScraping}</p>
-                <p>Mapping Failure: {row.original.toFKFailedMapping}</p>
-                <p>Validation Failure: {row.original.toFKFailedValidation}</p>
-              </div>
-            }
-          >
-            <div
-              className={`${
-                failRatio >= 3.0 ? "text-red-500" : "text-inherit"
-              }`}
-            >
-              {row.original.failCount} ({failRatio}%)
-            </div>
-          </TableCellTooltip>
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "diff-rate",
-    accessorKey: "getTotalDiffFound",
-    header: () => <TableHeadCustom className="w-32">DiffRate</TableHeadCustom>,
-    cell: ({ row }) => {
-      const diffRatio = row.original.diffRatio;
-      return (
-        <TableCellCustom>
-          <TableCellTooltip
-            tip={
-              <div>
-                <p>WithIn Cron: {row.original.diffRateCountWithInCron}</p>
-                <p>Above Cron: {row.original.diffRateCountAboveCron}</p>
-              </div>
-            }
-          >
-            <div
-              className={`${
-                diffRatio >= 10.0 ? "text-red-500" : "text-inherit"
-              }`}
-            >
-              {row.original.getTotalDiffFound} ({diffRatio}%)
-            </div>
-          </TableCellTooltip>
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "crawl-frequency",
-    accessorKey: "crawlFrequency",
-    header: () => <TableHeadCustom>Crawl Frequency</TableHeadCustom>,
-    cell: ({ row }) => {
-      return <TableCellCustom>{row.original.crawlFrequency}</TableCellCustom>;
-    },
-  },
-  {
-    id: "duration-to-launch",
-    accessorKey: "durationToLaunch",
-    header: () => <TableHeadCustom>Duration To Launch</TableHeadCustom>,
-    cell: ({ row }) => {
-      return <TableCellCustom>{row.original.durationToLaunch}</TableCellCustom>;
-    },
-  },
-  {
-    id: "deliver-count",
-    accessorKey: "deliverCount",
-    header: () => <TableHeadCustom>Deliver Count</TableHeadCustom>,
-    cell: ({ row }) => {
-      return <TableCellCustom>{row.original.deliverCount}</TableCellCustom>;
-    },
-  },
-  {
-    id: "close-count",
-    accessorKey: "closeCount",
-    header: () => <TableHeadCustom>Close Count</TableHeadCustom>,
-    cell: ({ row }) => {
-      return <TableCellCustom>{row.original.closeCount}</TableCellCustom>;
-    },
-  },
-  {
-    id: "fk-timeout",
-    accessorKey: "toFKFailedNotSent",
-    header: () => <TableHeadCustom>FK Timeout</TableHeadCustom>,
-    cell: ({ row }) => {
-      return (
-        <TableCellCustom>{row.original.toFKFailedNotSent}</TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "hit-rate",
-    accessorKey: "hitRateCount",
-    header: () => <TableHeadCustom>HitRate</TableHeadCustom>,
-    cell: ({ row }) => {
-      const hitPer = row.original.hitRatePer;
-      const queue = row.original.queueType;
-      return (
-        <TableCellCustom>
-          <TableCellTooltip
-            tip={
-              <div>
-                <p>WithIn Cron: {row.original.diffHitRateCountWithInCron}</p>
-                <p>Above Cron: {row.original.diffHitRateCountAboveCron}</p>
-              </div>
-            }
-          >
-            <div
-              className={`${hitPer >= 1.0 ? "text-red-500" : "text-inherit"}`}
-            >
-              {queue.includes("ADAPTIVE")
-                ? `${row.original.hitRateCount} (${hitPer}%)`
-                : "NA"}
-            </div>
-          </TableCellTooltip>
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "scheduler-id",
-    accessorKey: "schedulerId",
-    header: () => <TableHeadCustom>Scheduler Id</TableHeadCustom>,
-    cell: ({ row }) => {
-      return <TableCellCustom>{row.original.schedulerId}</TableCellCustom>;
-    },
-  },
-  {
-    id: "start-time",
-    accessorKey: "start_time",
-    header: () => <TableHeadCustom>Start Time</TableHeadCustom>,
-    cell: ({ row }) => {
-      return (
-        <TableCellCustom>
-          {format(toDate(row.original.start_time), "do MMM yyyy, HH:mm:ss")}
-        </TableCellCustom>
-      );
-    },
-  },
-  {
-    id: "end-time",
-    accessorKey: "end_time",
-    header: () => <TableHeadCustom>End Time</TableHeadCustom>,
-    cell: ({ row }) => {
-      return (
-        <TableCellCustom>
-          {format(toDate(row.original.end_time), "do MMM yyyy, HH:mm:ss")}
-        </TableCellCustom>
-      );
-    },
-  },
-];
-
 export function SummaryTable() {
   const username = React.useContext(UserContext);
   const params = useParams<ParamType>();
@@ -315,6 +33,309 @@ export function SummaryTable() {
       }
     });
   }
+
+  const columns: ColumnDef<SummaryType>[] = [
+    {
+      id: "carrier",
+      accessorKey: "jtCarrierCode",
+      header: () => <TableHeadCustom className="w-32">Carrier</TableHeadCustom>,
+      cell: ({ row }) => (
+        <TableCellCustom>{row.original.jtCarrierCode}</TableCellCustom>
+      ),
+      meta: {
+        className: "sticky left-0 bg-white",
+      },
+    },
+    {
+      id: "queue",
+      accessorKey: "queueType",
+      header: () => <TableHeadCustom>Queue</TableHeadCustom>,
+      cell: ({ row }) => {
+        const queue = row.original.queueType;
+        const qType = queue.includes("NORMAL")
+          ? "normal"
+          : queue.includes("ADAPTIVE")
+          ? "adaptive"
+          : queue.includes("RNF")
+          ? "rnf"
+          : "";
+
+        return (
+          <TableCellCustom>
+            <Badge className="capitalize bg-stone-500">{qType}</Badge>
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "active",
+      accessorKey: "jtCrawledTotal",
+      header: ({ column }) => {
+        return (
+          <TableHeadCustom
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Active <ArrowUpDown className="ml-2 h-4 w-4" />
+          </TableHeadCustom>
+        );
+      },
+      cell: ({ row }) => (
+        <TableCellCustom>{row.original.jtCrawledTotal}</TableCellCustom>
+      ),
+    },
+    {
+      id: "avg-age",
+      accessorKey: "AvgAge",
+      header: () => <TableHeadCustom>Avg Age</TableHeadCustom>,
+      cell: ({ row }) => {
+        const age = row.original.AvgAge;
+        return (
+          <TableCellCustom
+            className={`${
+              (params.mode === "ocean" && age >= 90) ||
+              (params.mode === "air" && age >= 14)
+                ? "text-red-500"
+                : "text-inherit"
+            }`}
+          >
+            {age} days
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "last-run",
+      accessorKey: "lastRunStartAt",
+      header: () => (
+        <TableHeadCustom className="w-32">Last Run</TableHeadCustom>
+      ),
+      cell: ({ row }) => (
+        <TableCellCustom>{row.original.lastRunStartAt} ago</TableCellCustom>
+      ),
+    },
+    {
+      id: "duration",
+      accessorKey: "timeDiffInFk",
+      header: () => <TableHeadCustom>Duration</TableHeadCustom>,
+      cell: ({ row }) => {
+        const durDiff = row.original.timeDiffMinutes;
+        return (
+          <TableCellCustom
+            className={`${
+              (params.mode === "ocean" && durDiff >= 90) ||
+              (params.mode === "air" && durDiff >= 60)
+                ? "text-red-500"
+                : "text-inherit"
+            }`}
+          >
+            {row.original.timeDiffInFk}
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "success",
+      accessorKey: "successCount",
+      header: () => <TableHeadCustom className="w-32">Success</TableHeadCustom>,
+      cell: ({ row }) => {
+        return (
+          <TableCellCustom>
+            {row.original.successCount} ({row.original.successRatio}%)
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "rnf",
+      accessorKey: "getReferenceNotFound",
+      header: () => <TableHeadCustom>RNF (404)</TableHeadCustom>,
+      cell: ({ row }) => {
+        const rnfRatio = row.original.getReferenceNotFoundPercentage;
+        const queue = row.original.queueType;
+        return (
+          <TableCellCustom
+            className={`${
+              ((params.mode === "ocean" && rnfRatio > 20) ||
+                (params.mode === "air" && rnfRatio > 5)) &&
+              !queue.includes("RNF")
+                ? "text-red-500"
+                : "text-inherit"
+            }`}
+          >
+            {row.original.getReferenceNotFound}
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "fail",
+      accessorKey: "failCount",
+      header: () => <TableHeadCustom className="w-32">Fail</TableHeadCustom>,
+      cell: ({ row }) => {
+        const failRatio = row.original.failureRatio;
+        return (
+          <TableCellCustom>
+            <TableCellTooltip
+              tip={
+                <div>
+                  <p>Sending Failure: {row.original.toFKFailedNotSent}</p>
+                  <p>API/Scraping Failure: {row.original.toFKFailedScraping}</p>
+                  <p>Mapping Failure: {row.original.toFKFailedMapping}</p>
+                  <p>Validation Failure: {row.original.toFKFailedValidation}</p>
+                </div>
+              }
+            >
+              <div
+                className={`${
+                  failRatio >= 3.0 ? "text-red-500" : "text-inherit"
+                }`}
+              >
+                {row.original.failCount} ({failRatio}%)
+              </div>
+            </TableCellTooltip>
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "diff-rate",
+      accessorKey: "getTotalDiffFound",
+      header: () => (
+        <TableHeadCustom className="w-32">DiffRate</TableHeadCustom>
+      ),
+      cell: ({ row }) => {
+        const diffRatio = row.original.diffRatio;
+        return (
+          <TableCellCustom>
+            <TableCellTooltip
+              tip={
+                <div>
+                  <p>WithIn Cron: {row.original.diffRateCountWithInCron}</p>
+                  <p>Above Cron: {row.original.diffRateCountAboveCron}</p>
+                </div>
+              }
+            >
+              <div
+                className={`${
+                  (params.mode === "ocean" && diffRatio >= 10.0) ||
+                  (params.mode === "air" && diffRatio >= 20.0)
+                    ? "text-red-500"
+                    : "text-inherit"
+                }`}
+              >
+                {row.original.getTotalDiffFound} ({diffRatio}%)
+              </div>
+            </TableCellTooltip>
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "crawl-frequency",
+      accessorKey: "crawlFrequency",
+      header: () => <TableHeadCustom>Crawl Frequency</TableHeadCustom>,
+      cell: ({ row }) => {
+        return <TableCellCustom>{row.original.crawlFrequency}</TableCellCustom>;
+      },
+    },
+    {
+      id: "duration-to-launch",
+      accessorKey: "durationToLaunch",
+      header: () => <TableHeadCustom>Duration To Launch</TableHeadCustom>,
+      cell: ({ row }) => {
+        return (
+          <TableCellCustom>{row.original.durationToLaunch}</TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "deliver-count",
+      accessorKey: "deliverCount",
+      header: () => <TableHeadCustom>Deliver Count</TableHeadCustom>,
+      cell: ({ row }) => {
+        return <TableCellCustom>{row.original.deliverCount}</TableCellCustom>;
+      },
+    },
+    {
+      id: "close-count",
+      accessorKey: "closeCount",
+      header: () => <TableHeadCustom>Close Count</TableHeadCustom>,
+      cell: ({ row }) => {
+        return <TableCellCustom>{row.original.closeCount}</TableCellCustom>;
+      },
+    },
+    {
+      id: "fk-timeout",
+      accessorKey: "toFKFailedNotSent",
+      header: () => <TableHeadCustom>FK Timeout</TableHeadCustom>,
+      cell: ({ row }) => {
+        return (
+          <TableCellCustom>{row.original.toFKFailedNotSent}</TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "hit-rate",
+      accessorKey: "hitRateCount",
+      header: () => <TableHeadCustom>HitRate</TableHeadCustom>,
+      cell: ({ row }) => {
+        const hitPer = row.original.hitRatePer;
+        const queue = row.original.queueType;
+        return (
+          <TableCellCustom>
+            <TableCellTooltip
+              tip={
+                <div>
+                  <p>WithIn Cron: {row.original.diffHitRateCountWithInCron}</p>
+                  <p>Above Cron: {row.original.diffHitRateCountAboveCron}</p>
+                </div>
+              }
+            >
+              <div
+                className={`${hitPer >= 1.0 ? "text-red-500" : "text-inherit"}`}
+              >
+                {queue.includes("ADAPTIVE")
+                  ? `${row.original.hitRateCount} (${hitPer}%)`
+                  : "NA"}
+              </div>
+            </TableCellTooltip>
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "scheduler-id",
+      accessorKey: "schedulerId",
+      header: () => <TableHeadCustom>Scheduler Id</TableHeadCustom>,
+      cell: ({ row }) => {
+        return <TableCellCustom>{row.original.schedulerId}</TableCellCustom>;
+      },
+    },
+    {
+      id: "start-time",
+      accessorKey: "start_time",
+      header: () => <TableHeadCustom>Start Time</TableHeadCustom>,
+      cell: ({ row }) => {
+        return (
+          <TableCellCustom>
+            {format(toDate(row.original.start_time), "do MMM yyyy, HH:mm:ss")}
+          </TableCellCustom>
+        );
+      },
+    },
+    {
+      id: "end-time",
+      accessorKey: "end_time",
+      header: () => <TableHeadCustom>End Time</TableHeadCustom>,
+      cell: ({ row }) => {
+        return (
+          <TableCellCustom>
+            {format(toDate(row.original.end_time), "do MMM yyyy, HH:mm:ss")}
+          </TableCellCustom>
+        );
+      },
+    },
+  ];
 
   const summaryQuery = useSummaryQuery(
     username || "",
@@ -347,5 +368,7 @@ export function SummaryTable() {
     );
   }
 
-  return <TableDataStaticComponent data={summaryQuery.data} columns={columns} />;
+  return (
+    <TableDataStaticComponent data={summaryQuery.data} columns={columns} />
+  );
 }
